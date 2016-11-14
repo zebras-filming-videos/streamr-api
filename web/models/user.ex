186968febd2c38d3bf304b284e603038ebe388e6
Greg White <gkwhite95@gmail.com>
2016-11-14 15:06:31 -0500

    Creates user table

---
+++ web/models/user.ex
@@ -0,0 +1,12 @@
+defmodule User do
+  use Streamr.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
+end
