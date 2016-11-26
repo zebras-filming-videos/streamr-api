936915935eb658afff155bfe002bc39e3a1dbb69
Greg White <gkwhite95@gmail.com>
2016-11-26 10:45:21 -0600

    Add ability to confirm user password

---
+++ test/models/user_test.exs
@@ -0,0 +1,20 @@
+defmodule Streamr.UserTest do
+  use Streamr.ConnCase
+  alias Streamr.User
+  alias Streamr.Repo
+
+  import Streamr.Factory
+
+  describe ".find_and_confirm_password" do
+    test "returns the user, if credentials are valid" do
+      user = build(:user, password: nil) |> set_password("password") |> insert
+      assert {:ok, user} == User.find_and_confirm_password(user.email, "password")
+    end
+
+    test "returns nil if credentials are invalid" do
+      {:error, user} = User.find_and_confirm_password("invalid email", "invalid password")
+
+      refute user
+    end
+  end
+end
