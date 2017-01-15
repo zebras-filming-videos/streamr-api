904c083ce9edcdd778b48fbb7f073f667bde6a85
Greg White <gkwhite95@gmail.com>
2017-01-15 17:37:03 -0500

    Add Authentication plug

--- web/router.ex
+++ web/router.ex
@@ -11,7 +11,7 @@ defmodule Streamr.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
-    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
     plug Guardian.Plug.LoadResource
   end
 
