deccbb88210a06c64c16b963fe56163ef2c3d3b9
Greg White <gkwhite95@gmail.com>
2017-04-08 15:50:43 -0400

    Extract logic for generating cdn urls

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -3,8 +3,6 @@ defmodule Streamr.UserView do
   use JaSerializer.PhoenixView
   alias Streamr.{Repo, User, UserSubscription}
 
-  @cdn_url System.get_env("CLOUDFRONT_URL")
-
   attributes [:name, :email, :current_user_subscribed, :image_url]
 
   def current_user_subscribed(_user, %Plug.Conn{assigns: %{current_user: nil}}), do: false
@@ -16,9 +14,8 @@ defmodule Streamr.UserView do
     end
   end
 
-  def image_url(%User{image_s3_key: nil}, _conn), do: nil
-  def image_url(%User{image_s3_key: image_s3_key}, _conn) do
-    @cdn_url <> image_s3_key
+  def image_url(user, _conn) do
+    Streamr.UrlQualifier.cdn_url_for(user.image_s3_key)
   end
 
   def render("access_token.json", %{access_token: access_token}) do
