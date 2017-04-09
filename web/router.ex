b566d1aeb51d78ef52e650aaef860f602e33d81b
Greg White <gkwhite95@gmail.com>
2017-04-09 15:47:35 -0400

    Change vote route

--- web/router.ex
+++ web/router.ex
@@ -34,14 +34,14 @@ defmodule Streamr.Router do
     get "/streams/subscribed", StreamController, :subscribed
     resources "/streams", StreamController do
       resources "/comments", CommentController, only: [:index, :create]
-      resources "/votes", VoteController, only: [:create]
+      post "/my_vote", VoteController, :create
 
       post "/add_line", StreamController, :add_line
       post "/end", StreamController, :end_stream
     end
 
     resources "/comments", CommentController, only: [:delete] do
-      resources "/votes", VoteController, only: [:create]
+      post "/my_vote", VoteController, :create
     end
 
     resources "/topics", TopicController, only: [:index] do
