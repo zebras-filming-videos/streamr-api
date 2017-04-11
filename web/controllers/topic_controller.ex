13dd06e006e6b14add5bd421392f05951d6595f8
Greg White <gkwhite95@gmail.com>
2017-04-10 21:19:50 -0400

    Fix things

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,7 +3,7 @@ defmodule Streamr.TopicController do
   alias Streamr.{Topic, Repo}
 
   def index(conn, params) do
-    topics = Topic |> Topic.ordered
+    topics = Topic |> Topic.ordered |> Repo.all()
 
     render(conn, "index.json-api", data: topics)
   end
