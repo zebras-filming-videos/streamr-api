9d638ee3796c28898847fcd0bf01cdf24e10e2d5
Greg White <gkwhite95@gmail.com>
2017-04-15 21:12:03 -0400

    Serialize all color attributes

--- web/views/color_view.ex
+++ web/views/color_view.ex
@@ -2,7 +2,7 @@ defmodule Streamr.ColorView do
   use Streamr.Web, :view
   use JaSerializer.PhoenixView
 
-  attributes [:hex]
+  attributes [:hex, :normal, :deuteranopia, :tritanopia, :protanopia]
 
   def hex(color, %{assigns: %{current_user: nil}}), do: color.normal
   def hex(color, %{assigns: %{current_user: user}}), do: Map.get(color, user.color_preference)
