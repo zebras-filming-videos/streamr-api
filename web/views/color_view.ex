55a9b748b6b526ab77e83e2cfb1d4d4fd761201b
Greg White <gkwhite95@gmail.com>
2017-04-15 21:07:32 -0400

    Serialize colors based on the user preference

--- web/views/color_view.ex
+++ web/views/color_view.ex
@@ -2,5 +2,8 @@ defmodule Streamr.ColorView do
   use Streamr.Web, :view
   use JaSerializer.PhoenixView
 
-  attributes [:normal, :deuteranopia, :tritanopia, :protanopia, :order]
+  attributes [:hex]
+
+  def hex(color, %{assigns: %{current_user: nil}}), do: color.normal
+  def hex(color, %{assigns: %{current_user: user}}), do: Map.get(color, user.color_preference)
 end
